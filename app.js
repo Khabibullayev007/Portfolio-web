5515b8bbc81063237c4cc463ec2bdeac528d865e
Habibullayev Sardorbek <microsofn@Habibullayev-Sardorbek.local>
2025-12-14 21:09:32 -0800

    Describe what you changed

---
+++ app.js
@@ -0,0 +1,41 @@
+const reveals = document.querySelectorAll("#Skills");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+const reveals2 = document.querySelectorAll("#About_me");
+
+window.addEventListener("scroll", () => {
+  reveals2.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+const reveals3 = document.querySelectorAll("#My_projects");
+
+window.addEventListener("scroll", () => {
+  reveals3.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+});
+
+const hero = document.querySelector(".main_page");
+
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 20;
+  const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+  hero.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+
+
+
